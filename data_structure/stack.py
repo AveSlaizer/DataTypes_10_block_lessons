936e7c46a95222280e2b96c3d198f8d474f9a082
AS. Sushkov <aveslaizer@gmial.com>
2023-05-16 20:03:50 +0300

    Написал и протестировал класс Stack

--- data_structure/stack.py
+++ data_structure/stack.py
@@ -1,7 +1,31 @@
 class Stack:
+
+    def __init__(self):
+        self.__data = []
+
+    @staticmethod
+    def __is_integer(item):
+        if not isinstance(item, int):
+            raise ValueError(f"Недопустимый тип данных '{item.__class__.__name__}', ожидался 'int'")
+
+    def push(self, item: int):
+        self.__is_integer(item)
+        self.__data.append(item)
+
+    def pop(self):
+        return self.__data.pop()
+
+    def peek(self):
+        return self.__data[-1]
+
+    def size(self):
+        return self.__data.__len__()
+
+    def is_empty(self):
+        return self.size() == 0
+
     # push – добавление нового элемента в стек.
     # pop – удаление и возврат очередного элемента в порядке «последним пришел, первым вышел» (Last In First Out – LIFO)
     # peek – возврат очередного элемента в порядке «последним пришел, первым вышел» (LIFO).
     # size  –  возврат количества элементов в стеке (будет использоваться метод __len__).
     # is_empty – возврат True, если в стеке нет элементов, иначе возврат False
-    pass
--- main.py
+++ main.py
@@ -1,5 +1,17 @@
+from data_structure.stack import Stack
+
+
 def execute_application():
-    pass
+    stack = Stack()
+
+    stack.push(11)
+    stack.push(22)
+    stack.push(33)
+    print(stack.size())
+    print(stack.pop())
+    print(stack.size())
+    print(stack.peek())
+    print(stack.is_empty())
 
 
 if __name__ == "__main__":
