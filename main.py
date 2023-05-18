d07a176f3d204c1f68fad158d7d43a58757f9322
AS. Sushkov <aveslaizer@gmial.com>
2023-05-18 19:03:24 +0300

    Добавил данные из гита

--- main.py
+++ main.py
@@ -1,30 +1,8 @@
 from data_structure.stack import Stack
 
 
-def is_correct_brackets(sequence: str):
-    """"""
-    brackets_dict = {
-        "(": ")",
-        "[": "]",
-        "{": "}",
-        "<": ">"
-    }
-    stack = Stack()
-
-    for symbol in sequence:
-        if symbol in brackets_dict.keys():
-            stack.push(symbol)
-        elif not stack.is_empty() and symbol == brackets_dict[stack.peek()]:
-            stack.pop()
-        else:
-            return False
-    if stack.is_empty():
-        return True
-    return False
-
-
 def execute_application():
-    print(eval("8+3*2+7*(6+3*4)"))
+    pass
 
 
 if __name__ == "__main__":
